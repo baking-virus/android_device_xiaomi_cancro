f3459d84515e9e59c44c22590d284866a41de6fa
baking-virus <mik3_xcid@rediffmail.com>
2015-11-13 22:50:54 +0530

    Update and rename omni_cancro.mk to broken.mk

--- broken.mk
+++ broken.mk
@@ -20,18 +20,18 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 # Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, vendor/broken/config/gsm.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/broken/config/common_mini_phone.mk)
 # must be before including omni part
 TARGET_BOOTANIMATION_SIZE := 720x480
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/broken/config/common.mk)
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/xiaomi/cancro/device.mk)
 # Discard inherited values and use our own instead.
 
-PRODUCT_NAME := omni_cancro
+PRODUCT_NAME := broken_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
